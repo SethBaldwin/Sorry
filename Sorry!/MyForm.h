bff343566619095d9151bc3411d9d73873a83b0f
SethBaldwin <sethnbaldwin@aol.com>
2014-04-07 16:54:22 -0400

    Creation of all cells

--- Sorry!/MyForm.h
+++ Sorry!/MyForm.h
@@ -5,8 +5,8 @@
 #include "Cell.h"
 
 
-int Board = 592; // Must be Divisible by 16.
-int CellSize = Board/16;
+int Board = 592; // Must be Divisible by 16. Cur = 592
+int CellSize = Board/16; // = 37
 
 int lastOc; //Stores last occupied pawn number for mouse up 
 
@@ -14,9 +14,21 @@ int mouseX,mouseY; // Get Click location
 
 vector<Cell> b;
 
+vector<Cell> red_Start;
+vector<Cell> red_Home;
+
+vector<Cell> blue_Start;
+vector<Cell> blue_Home;
+
+vector<Cell> yellow_Start;
+vector<Cell> yellow_Home;
+
+vector<Cell> green_Start;
+vector<Cell> green_Home;
+
 
 void MakeBoard();
-
+void MakeStart();
 
 namespace Sorry {
 
@@ -115,6 +127,7 @@ namespace Sorry {
 	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
 
 				 MakeBoard();
+				 MakeStart();
 
 				 ClientSize = System::Drawing::Size(Board+2, Board+2);
 				 panel1->Size = System::Drawing::Size(Board+2, Board+2);
@@ -139,14 +152,33 @@ namespace Sorry {
 				 for (int i = 0; i < b.size(); i++)
 					 gp1 -> FillRectangle(convertColor(b[i].getColor()), b[i].getLoc_x()+2, b[i].getLoc_y()+2, CellSize-3,CellSize-3);
 
+
+				 for (int i = 0; i < red_Start.size(); i++)
+					 gp1 -> DrawRectangle(black_p, red_Start[i].getLoc_x(), red_Start[i].getLoc_y(),CellSize,CellSize);
+				 for (int i = 0; i < red_Home.size(); i++)
+					 gp1 -> DrawRectangle(black_p, red_Home[i].getLoc_x(), red_Home[i].getLoc_y(),CellSize,CellSize);
+
+				 for (int i = 0; i < blue_Start.size(); i++)
+					 gp1 -> DrawRectangle(black_p, blue_Start[i].getLoc_x(), blue_Start[i].getLoc_y(),CellSize,CellSize);
+				 for (int i = 0; i < blue_Home.size(); i++)
+					 gp1 -> DrawRectangle(black_p, blue_Home[i].getLoc_x(), blue_Home[i].getLoc_y(),CellSize,CellSize);
+
+				 for (int i = 0; i < yellow_Start.size(); i++)
+					 gp1 -> DrawRectangle(black_p, yellow_Start[i].getLoc_x(), yellow_Start[i].getLoc_y(),CellSize,CellSize);
+				 for (int i = 0; i < yellow_Home.size(); i++)
+					 gp1 -> DrawRectangle(black_p, yellow_Home[i].getLoc_x(), yellow_Home[i].getLoc_y(), CellSize,CellSize);
+
+				 for (int i = 0; i < green_Start.size(); i++)
+					 gp1 -> DrawRectangle(black_p, green_Start[i].getLoc_x(), green_Start[i].getLoc_y(),CellSize,CellSize);
+				 for (int i = 0; i < green_Home.size(); i++)
+					 gp1 -> DrawRectangle(black_p, green_Home[i].getLoc_x(), green_Home[i].getLoc_y(), CellSize,CellSize);
+
 				 draw -> Enabled = false;
 			 }
 
 	private: System::Void panel1_MouseUp(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
 				 mouseX = (e->X);
 				 mouseY = (e->Y);
-<<<<<<< HEAD
-=======
 
 				 int cl = 0;
 				 cl = getCell(mouseX,mouseY);
@@ -154,8 +186,6 @@ namespace Sorry {
 				 b[cl].setColor(5);
 				 draw -> Enabled = true;
 
-
->>>>>>> 04a45e204badf89fcdf4155ddc56d4661a728d24
 			 }
 
 };
@@ -173,7 +203,7 @@ Brush ^convertColor(int c)
 	else if (c == 4)
 		col = gcnew SolidBrush(Color::Green);
 	else if (c == 5)
-		col = gcnew SolidBrush(Color::Black);
+		col = gcnew SolidBrush(Color::Pink);
 
 	return col;
 }
@@ -222,6 +252,81 @@ void MakeBoard()
 	}
 
 
+}
+
+void MakeStart()
+{
+	///////////////////////// RED
+
+	red_Start.push_back(Cell (1, (Board-CellSize*5), (Board-CellSize*2), 0));
+	red_Start.push_back(Cell (1, (Board-CellSize*6)+(CellSize/2), (Board-CellSize*3)+(CellSize/2), 0));
+	red_Start.push_back(Cell (1, (Board-CellSize*5)+(CellSize/2), (Board-CellSize*3)+(CellSize/2), 0));
+	red_Start.push_back(Cell (1, (Board-CellSize*5), (Board-CellSize*3), 0));
+
+	red_Home.push_back(Cell (1, (Board-CellSize*3), (Board-CellSize*2), 0));
+	red_Home.push_back(Cell (1, (Board-CellSize*3), (Board-CellSize*3), 0));
+	red_Home.push_back(Cell (1, (Board-CellSize*3), (Board-CellSize*4), 0));
+	red_Home.push_back(Cell (1, (Board-CellSize*3), (Board-CellSize*5), 0));
+	red_Home.push_back(Cell (1, (Board-CellSize*3), (Board-CellSize*6), 0));
+
+	red_Home.push_back(Cell (1, (Board-CellSize*3), (Board-CellSize*8), 0));
+	red_Home.push_back(Cell (1, (Board-CellSize*4)+(CellSize/2), (Board-CellSize*8)+(CellSize/2), 0));
+	red_Home.push_back(Cell (1, (Board-CellSize*3)+(CellSize/2), (Board-CellSize*8)+(CellSize/2), 0));
+	red_Home.push_back(Cell (1, (Board-CellSize*3), (Board-CellSize*7), 0));
+
+	////////////////////////// BLUE
+
+	blue_Start.push_back(Cell (2, (Board-CellSize*15), (Board-CellSize*5), 0));
+	blue_Start.push_back(Cell (2, (Board-CellSize*15)+(CellSize/2), (Board-CellSize*6)+(CellSize/2), 0));
+	blue_Start.push_back(Cell (2, (Board-CellSize*15)+(CellSize/2), (Board-CellSize*5)+(CellSize/2), 0));
+	blue_Start.push_back(Cell (2, (Board-CellSize*14), (Board-CellSize*5), 0));
+
+	blue_Home.push_back(Cell (2, (Board-CellSize*15), (Board-CellSize*3), 0));
+	blue_Home.push_back(Cell (2, (Board-CellSize*14), (Board-CellSize*3), 0));
+	blue_Home.push_back(Cell (2, (Board-CellSize*13), (Board-CellSize*3), 0));
+	blue_Home.push_back(Cell (2, (Board-CellSize*12), (Board-CellSize*3), 0));
+	blue_Home.push_back(Cell (2, (Board-CellSize*11), (Board-CellSize*3), 0));
+
+	blue_Home.push_back(Cell (2, (Board-CellSize*10), (Board-CellSize*3), 0));
+	blue_Home.push_back(Cell (2, (Board-CellSize*10)+(CellSize/2), (Board-CellSize*3)+(CellSize/2), 0));
+	blue_Home.push_back(Cell (2, (Board-CellSize*10)+(CellSize/2), (Board-CellSize*4)+(CellSize/2), 0));
+	blue_Home.push_back(Cell (2, (Board-CellSize*9), (Board-CellSize*3), 0));
+
+	////////////////////////// YELLOW
+
+	yellow_Start.push_back(Cell (3, (Board-CellSize*12), (Board-CellSize*15), 0));
+	yellow_Start.push_back(Cell (3, (Board-CellSize*13)+(CellSize/2), (Board-CellSize*15)+(CellSize/2), 0));
+	yellow_Start.push_back(Cell (3, (Board-CellSize*12)+(CellSize/2), (Board-CellSize*15)+(CellSize/2), 0));
+	yellow_Start.push_back(Cell (3, (Board-CellSize*12), (Board-CellSize*14), 0));
+
+	yellow_Home.push_back(Cell (3, (Board-CellSize*14), (Board-CellSize*15), 0));
+	yellow_Home.push_back(Cell (3, (Board-CellSize*14), (Board-CellSize*14), 0));
+	yellow_Home.push_back(Cell (3, (Board-CellSize*14), (Board-CellSize*13), 0));
+	yellow_Home.push_back(Cell (3, (Board-CellSize*14), (Board-CellSize*12), 0));
+	yellow_Home.push_back(Cell (3, (Board-CellSize*14), (Board-CellSize*11), 0));
+
+	yellow_Home.push_back(Cell (3, (Board-CellSize*14), (Board-CellSize*10), 0));
+	yellow_Home.push_back(Cell (3, (Board-CellSize*14)+(CellSize/2), (Board-CellSize*10)+(CellSize/2), 0));
+	yellow_Home.push_back(Cell (3, (Board-CellSize*14)-(CellSize/2), (Board-CellSize*10)+(CellSize/2), 0));
+	yellow_Home.push_back(Cell (3, (Board-CellSize*14), (Board-CellSize*9), 0));
+
+	////////////////////////// GREEN
+
+	green_Start.push_back(Cell (4, (Board-CellSize*2), (Board-CellSize*12), 0));
+	green_Start.push_back(Cell (4, (Board-CellSize*3)+(CellSize/2), (Board-CellSize*13)+(CellSize/2), 0));
+	green_Start.push_back(Cell (4, (Board-CellSize*3)+(CellSize/2), (Board-CellSize*12)+(CellSize/2), 0));
+	green_Start.push_back(Cell (4, (Board-CellSize*3), (Board-CellSize*12), 0));
+
+	green_Home.push_back(Cell (4, (Board-CellSize*2), (Board-CellSize*14), 0));
+	green_Home.push_back(Cell (4, (Board-CellSize*3), (Board-CellSize*14), 0));
+	green_Home.push_back(Cell (4, (Board-CellSize*4), (Board-CellSize*14), 0));
+	green_Home.push_back(Cell (4, (Board-CellSize*5), (Board-CellSize*14), 0));
+	green_Home.push_back(Cell (4, (Board-CellSize*6), (Board-CellSize*14), 0));
+
+	green_Home.push_back(Cell (4, (Board-CellSize*8), (Board-CellSize*14), 0));
+	green_Home.push_back(Cell (4, (Board-CellSize*8)+(CellSize/2), (Board-CellSize*14)+(CellSize/2), 0));
+	green_Home.push_back(Cell (4, (Board-CellSize*8)+(CellSize/2), (Board-CellSize*15)+(CellSize/2), 0));
+	green_Home.push_back(Cell (4, (Board-CellSize*7), (Board-CellSize*14), 0));
 }
 
 
